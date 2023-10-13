21e50c8968c55c25bdf0a95b4f56ef344e35155a
Samuel Chamalé <samuel.chamale23@gmail.com>
2023-10-13 15:31:34 -0600

    feat: send email to reported

--- controllers/reportController.js
+++ controllers/reportController.js
@@ -11,17 +11,21 @@ const createReport = async (req, res) => {
         const report = await Report.create(req.body)
 
         var reported
+        var reportedEmail
         const type = req.body.type
 
         if (type == 'Event') {
             // Get event title
-            reported = await Event.findById(req.body.eventId).select('title')
+            reported = await Event.findById(req.body.eventId).populate(
+                'creator',
+                'email'
+            )
+            reportedEmail = reported.creator.email
             reported = reported.title
         } else if (type == 'User') {
             // Get user username
-            reported = await User.findById(req.body.perpetrator).select(
-                'username'
-            )
+            reported = await User.findById(req.body.perpetrator)
+            reportedEmail = reported.email
             reported = reported.username
         }
 
@@ -62,6 +66,45 @@ const createReport = async (req, res) => {
                 </table>
                 <p style="color: #666666;margin-bottom: 30px">
                   Nos comunicaremos nuevamente en cuanto tengamos noticias de tu reporte.
+                </p>
+                <p style="color: ##666666; opacity: 0.7; width: 90%;">
+                  Si deseas mas información puedes contactarte con nuestro equipo a <a href="mailto:help@overcome.tech">help@overcome.tech</a>
+                </p>
+              </td>
+            </tr>
+          </table>
+      `,
+        })
+
+        await resend.emails.send({
+            from: 'Overcome <sender@app-overcome.schr.tech>',
+            to: [reportedEmail],
+            subject: 'Tienes un nuevo reporte ⚠️',
+            html: `
+            <table width="100%"  cellpadding="0" cellspacing="0"  style="background-color: #ffffff; border-radius: 4px; padding: 20px;margin-top: 1rem">
+            <tr>
+              <td align="center">
+                <img src='https://drive.google.com/uc?id=1t2AKc12EanKhfjs_dIqXgXFk-ySY9l4x' style="height:100px"/>
+                <h1 style="color: #333333; font-size:29px">Hola, <a href="mailto:${reportedEmail}">${reportedEmail}</a>:</h1>
+                <p style="color: #666666;">Hemos recibido un reporte en <a href='https://app-overcome.onrender.com' target="_blank">Overcome</a> sobre algo relacionado a ti.
+                  Los reportes son valiosos para mantener una comunidad sana y respetuosa para todos. 
+                </p>
+                <br/>   
+                 <p style="color: #666666;">
+                  A continuación, encontrarás un resumen del reporte mencionado:
+                </p>
+                <table style="color: #666666; margin-bottom: 10px">
+                  <tr>
+                    <td style="padding:5px; font-weight: bold">Fué reportado (${type}): </td>
+                    <td>${reported}</td>
+                  </tr>
+                  <tr>
+                    <td style="padding:5px; font-weight: bold">Hora de reporte: </td>
+                    <td>${reportDate}</td>
+                  </tr>
+                </table>
+                <p style="color: #666666;margin-bottom: 30px">
+                  Nos comunicaremos nuevamente en cuanto tengamos noticias del reporte.
                 </p>
                 <p style="color: ##666666; opacity: 0.7; width: 90%;">
                   Si deseas mas información puedes contactarte con nuestro equipo a <a href="mailto:help@overcome.tech">help@overcome.tech</a>
@@ -136,7 +179,7 @@ const deleteEventReports = async (eventId) => {
               <td align="center">
                 <img src='https://drive.google.com/uc?id=1t2AKc12EanKhfjs_dIqXgXFk-ySY9l4x' style="height:100px"/>
                 <h1 style="color: #333333; font-size:29px">Hola, <a href="mailto:${emailOwner}">${emailOwner}</a>:</h1>
-                <p style="color: #666666;">Hace tiempo recibimos un reporte en <a href='https://app-overcome.onrender.com' target="_blank">Overcome</a>, sobre un evento que creaste. 
+                <p style="color: #666666;">Hace tiempo recibimos un reporte en <a href='https://app-overcome.onrender.com' target="_blank">Overcome</a> sobre un evento que creaste. 
                   <br/>
                   Los reportes son valiosos para mantener una comunidad sana y respetuosa para todos. 
                 </p>
